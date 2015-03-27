f75c38142db1558089d84333f6b8a6bde7c90523
Frode Danielsen <frode@e5r.no>
2015-03-27 19:12:56 +0100

    Fix bug overwriting the whole app JS tree

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ module.exports = {
     name: 'ember-cli-po2json',
 
     setupPreprocessorRegistry: function (type, registry) {
+        var self = this;
         registry.add('js', {
             name: 'ember-cli-po2json',
             ext: 'po',
             toTree: function (tree, inputPath, outputPath) {
-                return poCompile(tree, {
+                var localeTree = poCompile(tree, {
                     destDir: outputPath + '/locale',
                     srcDir: '/' + registry.app.name,
                     es6: true
                 });
+
+                return self.mergeTrees([tree, localeTree]);
             }
         });
     },
