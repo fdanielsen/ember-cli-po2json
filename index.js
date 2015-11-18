ea1e547d31e284db9ddc5c81c4297290f2f9fc3a
Frode Danielsen <frode@e5r.no>
2015-11-18 18:26:35 +0100

    Run after ember-cli-babel and support newest broccoli-po2json

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ module.exports = {
 
     setupPreprocessorRegistry: function (type, registry) {
         var self = this;
+
         registry.add('js', {
             name: 'ember-cli-po2json',
             ext: 'po',
             toTree: function (tree, inputPath, outputPath) {
-                var localeTree = poCompile(tree, {
+                var localeTree = poCompile([tree], {
                     destDir: outputPath + '/locale',
                     srcDir: '/' + registry.app.name,
                     es6: true
